d2f40df4fa0cd357605ae202489ba9f876d1632b
Fadacoal <arjeandequiroz@gmail.com>
2024-04-16 14:27:49 +0800

    Added Unit Tests * withoutPercentageAndRoundUp

--- app/src/test/java/com/example/a04152024activity/TestingLogic.kt
+++ app/src/test/java/com/example/a04152024activity/TestingLogic.kt
@@ -3,9 +3,7 @@ package com.example.a04152024activity
 import org.junit.Assert
 import org.junit.Test
 import java.text.NumberFormat
-
 class TestingLogic {
-
     @Test
     fun TestingTheCalculateLogic(){
         val amount = 10.00
@@ -14,9 +12,7 @@ class TestingLogic {
 
         val actualTip = calculateTip(amount = amount, tipPercent = percentage, false)
         Assert.assertEquals(expectedTip, actualTip)
-
     }
-
     @Test
     fun TestingTheCalculateLogicRoundUp(){
         val amount = 12.00
@@ -24,6 +20,15 @@ class TestingLogic {
         val expectedTip = NumberFormat.getCurrencyInstance().format(3)
 
         val actualTip = calculateTip(amount = amount, tipPercent = percentage, true)
+        Assert.assertEquals(expectedTip, actualTip)
+
+    }
+    @Test
+    fun withoutPercentageAndRoundUp(){
+        val amount = 10.00
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+
+        val actualTip = calculateTip(amount = amount, roundUp = true)
         Assert.assertEquals(expectedTip, actualTip)
 
     }
